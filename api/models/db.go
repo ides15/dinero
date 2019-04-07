287b767bd75e1cab7e465fb86e35cdb8ba155898
John Ide <ide.johnc@gmail.com>
2019-04-07 15:47:21 -0400

    go mod init, starting db testing

--- api/models/db.go
+++ api/models/db.go
@@ -7,6 +7,35 @@ import (
 	_ "github.com/mattn/go-sqlite3"
 )
 
+const (
+	usersTableStmt = `
+	CREATE TABLE IF NOT EXISTS "users" (
+		"id" INTEGER,
+		"first_name" TEXT NOT NULL,
+		"last_name" TEXT NOT NULL,
+		"full_name" TEXT NOT NULL,
+		"email" TEXT NOT NULL UNIQUE,
+		"biweekly_income" REAL NOT NULL,
+		
+		PRIMARY KEY("id")
+	)`
+	accountsTableStmt = `
+	CREATE TABLE IF NOT EXISTS "accounts" (
+		"id" INTEGER,
+		"user_id" INTEGER NOT NULL,
+		"name" TEXT NOT NULL,
+		"account_type" TEXT NOT NULL,
+		"minimum_payment" REAL NOT NULL,
+		"current_payment" REAL NOT NULL,
+		"full_amount" REAL NOT NULL,
+		"due_date" TEXT NOT NULL,
+		"url" TEXT NOT NULL,
+
+		UNIQUE("user_id", "name")
+		PRIMARY KEY("id")
+	)`
+)
+
 // Store is a general interface for a datastore (real vs mock)
 type Store interface {
 	AllAccounts() ([]*Account, error)
@@ -34,7 +63,7 @@ func InitDB(dbName string) (*DB, error) {
 	}
 
 	if err = db.Ping(); err != nil {
-		return nil, err
+		return nil, ErrBadPing
 	}
 
 	err = createUsersTable(db)
@@ -50,17 +79,7 @@ func InitDB(dbName string) (*DB, error) {
 }
 
 func createUsersTable(db *sql.DB) error {
-	stmt, err := db.Prepare(`
-	CREATE TABLE IF NOT EXISTS "users" (
-		"id" INTEGER,
-		"first_name" TEXT NOT NULL,
-		"last_name" TEXT NOT NULL,
-		"full_name" TEXT NOT NULL,
-		"email" TEXT NOT NULL UNIQUE,
-		"biweekly_income" REAL NOT NULL,
-		
-		PRIMARY KEY("id")
-	)`)
+	stmt, err := db.Prepare(usersTableStmt)
 	if err != nil {
 		return err
 	}
@@ -75,21 +94,7 @@ func createUsersTable(db *sql.DB) error {
 }
 
 func createAccountsTable(db *sql.DB) error {
-	stmt, err := db.Prepare(`
-	CREATE TABLE IF NOT EXISTS "accounts" (
-		"id" INTEGER,
-		"user_id" INTEGER NOT NULL,
-		"name" TEXT NOT NULL,
-		"account_type" TEXT NOT NULL,
-		"minimum_payment" REAL NOT NULL,
-		"current_payment" REAL NOT NULL,
-		"full_amount" REAL NOT NULL,
-		"due_date" TEXT NOT NULL,
-		"url" TEXT NOT NULL,
-
-		UNIQUE("user_id", "name")
-		PRIMARY KEY("id")
-	)`)
+	stmt, err := db.Prepare(accountsTableStmt)
 	if err != nil {
 		return err
 	}
--- api/models/errors.go
+++ api/models/errors.go
@@ -4,5 +4,8 @@ import "errors"
 
 var (
 	// ErrNotFound is an error creator for models where DB query results return nothing
-	ErrNotFound = errors.New("Error: no record(s) found")
+	ErrNotFound = errors.New("error: no record(s) found")
+	// ErrBadPing is an error creator for the DB model where the application errors in
+	// pinging the database
+	ErrBadPing = errors.New("error: cannot ping database")
 )
